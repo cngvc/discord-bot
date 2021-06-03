7ed92cd5d8357ff519859e1034935bc2caaba318
JoeVu <joevu98@gmail.com>
2021-06-03 11:06:18 +0700

    add more gif

--- commands/basicResponses.js
+++ commands/basicResponses.js
@@ -5,7 +5,18 @@ const PHAN_VAN_IMAGE_URL = "https://i.ibb.co/8mHwCv5/phan-van.png";
 const X_PAIN_IMAGE_URL = "https://i.ibb.co/QFDhn4G/x-pain.png";
 const PRAY_GIF_URL = "http://gph.is/1rrG9vI";
 const THANKS_YOU_GIF_URL = "https://gph.is/g/EvdgPYo";
-const FUCK_YOU_GIF_URL = "http://gph.is/22z6xkE"
+const FUCK_YOU_GIF_URL = "https://media.giphy.com/media/KzGCAlMiK6hQQ/giphy.gif";
+const SCAM_ALERT_GIF_URLS = [
+  "https://i.ibb.co/ftqtTJV/tenor.gif",
+  "https://media.giphy.com/media/XQKBuQmfjt1xm/giphy.gif",
+  "https://media.giphy.com/media/2Y7nrBzXqhMpXubmJT/giphy.gif",
+  "https://media.giphy.com/media/giLdGtRdBDxYVMoUw0/giphy.gif"
+]
+const ANGRY_GIF_URLS = [
+  "https://media.giphy.com/media/AIuTKC0WnCgw0/giphy.gif",
+  "https://media.giphy.com/media/J2D7HjS6sOsnjSadcA/giphy.gif"
+]
+const X_TK_GIF_URL = "https://media.giphy.com/media/RoS4JYcw0RvK8/giphy.gif"
 
 module.exports = [
   {
@@ -34,10 +45,8 @@ module.exports = [
   },
   {
     match(content) {
-      return (
-        content.toLowerCase().includes("phan van") ||
-        content.toLowerCase().includes("phân vân")
-      );
+      const strings = ["phan van", "phân vân"]
+      return strings.some(srt => content.toLowerCase().includes(srt));
     },
     execute(channel) {
       channel.send(PHAN_VAN_IMAGE_URL);
@@ -45,10 +54,8 @@ module.exports = [
   },
   {
     match(content) {
-      return (
-        content.toLowerCase().includes("future") ||
-        content.toLowerCase().includes("shitcoin")
-      );
+      const strings = ["futu", "future", "shitcoin"]
+      return strings.some(srt => content.toLowerCase().includes(srt));
     },
     execute(channel) {
       channel.send(X_PAIN_IMAGE_URL);
@@ -56,10 +63,8 @@ module.exports = [
   },
   {
     match(content) {
-      return (
-        content.toLowerCase().includes("please") ||
-        content.toLowerCase().includes("pls")
-      );
+      const strings = ["please", "pls"]
+      return strings.some(srt => content.toLowerCase().includes(srt));
     },
     execute(channel) {
       channel.send(PRAY_GIF_URL);
@@ -67,12 +72,8 @@ module.exports = [
   },
   {
     match(content) {
-      return (
-        content.toLowerCase().includes("thanks") ||
-        content.toLowerCase().includes("tks") ||
-        content.toLowerCase().includes("cảm ơn") ||
-        content.toLowerCase().includes("cam on")
-      );
+      const strings = ["thanks", "tks", "cam on", "cảm ơn"]
+      return strings.some(srt => content.toLowerCase().includes(srt));
     },
     execute(channel) {
       channel.send(THANKS_YOU_GIF_URL);
@@ -80,16 +81,37 @@ module.exports = [
   },
   {
     match(content) {
-      return (
-        content.toLowerCase().includes("fuck") ||
-        content.toLowerCase().includes("dit me may") ||
-        content.toLowerCase().includes("địt mẹ mày") ||
-        content.toLowerCase().includes("dcmm") || 
-        content.toLowerCase().includes("dcm")
-      );
+      const strings = ["fuck", "dit", "địt", "dcm"]
+      return strings.some(srt => content.toLowerCase().includes(srt));
     },
     execute(channel) {
       channel.send(FUCK_YOU_GIF_URL);
+    },
+  },
+  {
+    match(content) {
+      return content.toLowerCase().includes("scam");
+    },
+    execute(channel) {
+      const result = SCAM_ALERT_GIF_URLS[Math.floor(Math.random() * SCAM_ALERT_GIF_URLS.length)]
+      channel.send(result);
+    },
+  },
+  {
+    match(content) {
+      return content.toLowerCase().includes("dỗi");
+    },
+    execute(channel) {
+      const result = ANGRY_GIF_URLS[Math.floor(Math.random() * ANGRY_GIF_URLS.length)]
+      channel.send(result);
+    },
+  },
+  {
+    match(content) {
+      return /^x[\d]{2,}g/.test(content);
+    },
+    execute(channel) {
+      channel.send(X_TK_GIF_URL);
     },
   },
 ];
